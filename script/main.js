d1f99bae378a29ee4c8ce1c0ba237328f4a78c39
john <netraveler89@gmail.com>
2022-06-18 02:59:05 -0400

    created food functions

--- script/main.js
+++ script/main.js
@@ -5,16 +5,40 @@
 let food = [];
 
 
+
+
 // Food Item Factory Funciton
-const FoodItem = function(biome, shelfLife, name, type) {
+const FoodItem = function(shelfLife, name, type, ...args) {
     this.type = type;
     this.name = name;
-    this.biome = biome;
     this.shelfLife = shelfLife;
+    this.biomes = args;
 
 };
-// adds
-function addToFood(arg) {
-    food.splice(1, 0, arg);
+// adds a FoodItem object to the food array
+function addToFood(product) {
+    food.splice(1, 0, product);
     return food;
+}
+
+
+function foodTable() {
+    // loops through food array
+    let foodDisplay = document.getElementById("foodDisplay");
+
+    let type = document.createElement("p");
+    let name = document.createElement("p");
+    let shelfLife = document.createElement("p");
+
+    for (i = 0; i < food.length; i++) {
+
+        type.innerHTML = food[i].type;
+        foodDisplay.appendChild(type);
+
+        name.innerHTML = food[i].name;
+        foodDisplay.appendChild(name);
+
+        shelfLife.innerHTML = food[i].shelfLife;
+        foodDisplay.appendChild(shelfLife);
+    }
 }
